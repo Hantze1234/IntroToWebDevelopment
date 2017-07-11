793a51398fe57033b2591879d08db3f945aa9298
John Boyle <john.boyle@comcast.net>
2017-07-11 06:56:51 -0400

    Hello World

---
+++ HelloGuild/src/main/java/HelloGuild.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author n0217184
+ */
+public class HelloGuild {
+    public static void main (String[] args) {
+		System.out.println("Hello, Guild!");
+	}
+}
